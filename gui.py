d2e46baf1336677c94924157f592bf3bf3eb15aa
Kanishak Sangwan <112748208+Kan1shak@users.noreply.github.com>
2024-12-08 07:38:30 +0530

    implement toolbar functionality; add 404 page

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
-# TODO: Make the toolabr buttons functional
-# TODO: Make the address bar on the toolbar functional
+# TODO: Make the bookmark button functional
 # TODO: Add a settings page
 # TODO: Work on reflections page
 
@@ -30,6 +29,41 @@ def get_or_set(domain:str, key:str, callback, **kwargs):
         r.set(f"{domain}:{key}",json.dumps(value))
         return value
 
+def not_found(request:Request,exc:Exception):
+    # if its a htmx AJAX request, we return only the content
+    if request.headers.get('HX-Request') == 'true':
+        return (
+            Title('404 - Not Found | HalluciNet'),
+            toolbar('hallucinet.hln/404'),
+            Div(
+                H1('404 - Not Found', cls='title'),
+                P('The page you are looking for does not exist.', cls='subtitle'),
+                cls='header'
+            )
+        )
+    else:
+        return (
+            Link(rel='stylesheet', href='css/homepage.css'),
+            Link(rel='stylesheet', href='css/search.css'),
+            Link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=BhuTuka+Expanded+One&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap'),
+            Title('404 - Not Found | HalluciNet'),
+            Main(
+                toolbar('hallucinet.hln/404'),
+                Div(
+                    Div(
+                        H1('404 - Not Found', cls='title'),
+                        P('The page you are looking for does not exist.', cls='subtitle'),
+                        cls='four-o-four'
+                    ),
+                    cls='content-view'
+                ),
+            )
+        )
+
+exception_handlers = {
+    404: not_found
+}
+
 app, rt = fast_app(
     pico=False,static_path='static',
     hdrs=(Link(rel='stylesheet', href='https://www.nerdfonts.com/assets/css/webfont.css'),
@@ -37,8 +71,10 @@ app, rt = fast_app(
         Link(rel='stylesheet', href='css/global.css'),
         Link(rel='preconnect', href='hhttps://fonts.googleapis.com'),
         Link(rel='preconnect', href='https://fonts.gstatic.com', crossorign=True),
-    )
+    ),
+    exception_handlers=exception_handlers
 )
+
 
 def nav():
     nav = Ul(
@@ -49,37 +85,40 @@ def nav():
     )
     return Nav(nav)
 
-def toolbar():
+def toolbar(address='hallucinet.hln'):
     toolbar = Div(
         Div(
             Div(
-                Button(I(cls='nf nf-fa-angle_left'), title='Go Back', cls='navigation-button'),
-                Button(I(cls='nf nf-fa-angle_right'), title='Go Forward', cls='navigation-button'),
+                Button(I(cls='nf nf-fa-angle_left'), title='Go Back', cls='navigation-button',onclick="window.history.back()"),
+                Button(I(cls='nf nf-fa-angle_right'), title='Go Forward', cls='navigation-button', onclick="window.history.forward()"),
                 cls='navigation-buttons',
             ),
             Div(
-                Button(I(cls='nf nf-md-refresh'), title='Refresh Page', cls='navigation-button'),
-                Button(I(cls='nf nf-fa-home'),title='Home Page', cls='navigation-button'),
+                Button(I(cls='nf nf-md-refresh'), title='Refresh Page', cls='navigation-button', hx_get='/refresh',
+                hx_vals='js:{"url":window.location.href}'),
+                Button(I(cls='nf nf-fa-home'),title='Home Page', cls='navigation-button', hx_get='/', hx_target='.content-view',hx_push_url="true"),
                 cls='navigation-buttons'
             ),
             cls='navigation-buttons-container'
         ),
         Div(
-            Input(type='text', value='hallucinet.start',placeholder='Search HalluciNet or type a HRL', cls='address-bar'),
+            Input(type='text',value=address,placeholder='Search HalluciNet or type a HRL', cls='address-bar',
+            hx_get="/resolvehrl", hx_vals='js:{"url":document.querySelector(".address-bar").value}', hx_target='.content-view', hx_push_url="true",
+            hx_trigger="keyup[key=='Enter']"),
             cls='navigation-buttons address-bar-container'
         ),            
         Div(
             Button(I(cls='nf nf-md-bookmark_outline'), title='Bookamark', cls='navigation-button'),
             cls='navigation-buttons'
         ),
-        cls='toolbar'
+        cls='toolbar', id='toolbar',hx_swap_oob="true"
     )
     return toolbar
 
 def search_bar(prefill:str=None):
     return  Div(Input(type='text', placeholder='Search HalluciNet...', cls='search-bar', name='query',
             value=prefill,
-            hx_get='/search', hx_target='.content-view', hx_trigger="keyup[key=='Enter']"),
+            hx_get='/search', hx_target='.content-view', hx_trigger="keyup[key=='Enter']", hx_push_url="true"),
             cls='search-bar-container'
     )
 
@@ -104,21 +143,27 @@ def home_page():
 
 
 @app.route('/')
-def index():
-    return (
-        Link(rel='stylesheet', href='css/homepage.css'),
-        Link(rel='stylesheet', href='css/search.css'),
-        Link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=BhuTuka+Expanded+One&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap'),
-        Main(
-            toolbar(),
-            Div(
-                Title('HalluciNet'),
-                nav(),
-                home_page(),
-                cls='content-view'
-            ),
+def index(request:Request):
+    # if its a htmx AJAX request, we return only the content
+    if request.headers.get('HX-Request') == 'true':
+        return toolbar(),(Title('HalluciNet'),
+            nav(),
+            home_page())
+    else:
+        return (
+            Link(rel='stylesheet', href='css/homepage.css'),
+            Link(rel='stylesheet', href='css/search.css'),
+            Link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=BhuTuka+Expanded+One&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap'),
+            Title('HalluciNet'),
+            Main(
+                toolbar(),
+                Div(
+                    nav(),
+                    home_page(),
+                    cls='content-view'
+                ),
+            )
         )
-    )
 
 
 class SearchResult:
@@ -143,15 +188,60 @@ def search_results(query,h_results):
     return Div(search_results_top_bar, search_results, cls='search-results')
 
 @app.route('/search')
-def search(query:str):
+def search(query:str, request:Request):
     query = query.strip()
     results_j = get_or_set(domain='query',key=query,callback=searcher.search,query=query,max_results=10)
     results = results_j['results']
     results = [(result['title'],result['url'],result['description']) for result in results]
 
-    return (
+    url_path = request.url.path
+    encoded_params = request.url.query
+    display_url = f"hallucinet.hln{url_path}?{encoded_params}"
+
+    # if its a reload, we return with all the hearders
+    if request.headers.get('HX-Request') == 'true':
+        return (
+        Link(rel='stylesheet', href='css/search.css'),
+        toolbar(display_url),
         Title(f'{query} - Hallucinet'),
         search_results(query,results)
     )
+    else:
+        return (
+        Link(rel='stylesheet', href='css/homepage.css'),
+        Link(rel='stylesheet', href='css/search.css'),
+        Link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=BhuTuka+Expanded+One&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap'),
+        Title('HalluciNet'),
+        Main(
+            toolbar(display_url),
+            Div(
+                search_results(query,results),
+                cls='content-view'
+            ),
+        )
+    )
+
+@app.route('/refresh')
+def refresh(request:Request, url:str):
+    # if the url is of a search page, we clear the cache for that query
+    # works as expected, but commenting out for now
+    # if 'search' in url:
+    #     query = url.split('=')[-1]
+    #     r.delete(f"query:{query}")
+    
+    return Redirect(url)
+
+@app.route('/resolvehrl')
+def resolve_hrl(url:str):
+    if url == 'hallucinet.hln':
+        return Redirect('/')
+    elif 'search' in url:
+        query = url.split('=')[-1]
+        return Redirect(f'/search?query={query}')
+    elif r.exists(f"page:{url}"):
+        return json.loads(r.get(f"page:{url}"))
+    else:
+        return RedirectResponse('/404')
+
 
 serve()
